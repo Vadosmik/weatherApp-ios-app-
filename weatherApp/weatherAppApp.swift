c9b88976a3c45f9f0b3e5020652f1d490668de6b
Vadzim Mikanovich <liontv05@gmail.com>
2024-01-20 01:12:18 +0100

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Wadim Mikanowicz on 20/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
